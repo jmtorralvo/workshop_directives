c9324cac08e44cdc596794089c9410dd444308ba
jmtorralvo <thelastonealive@gmail.com>
2016-03-21 12:58:25 +0100

    fix: Ex 18

--- ejercicios/ejercicio18/ejercicio.js
+++ ejercicios/ejercicio18/ejercicio.js
@@ -3,6 +3,23 @@
     .module('directivas')
     .directive('seleccionSuperheroes', function ($q, $timeout){
       const ddo = {
+        template: `
+          <button type="button" ng-click="seleccion.assignHeroName('Wolverine')">Wolverine</button>
+          <button type="button" ng-click="seleccion.assignHeroName('Beast')">Beast</button>
+          <button type="button" ng-click="seleccion.heroe.nombre = 'Spiderman'">Spiderman</button>
+        `,
+        scope: {
+          heroe: '=ngModel'
+        },
+        controller: function () {
+          let seleccion = this;
+          seleccion.assignHeroName = name => {
+            seleccion.heroe = seleccion.heroe || {};
+            seleccion.heroe.nombre = name;
+          }
+        },
+        controllerAs: 'seleccion',
+        bindToController: true
       };
       return ddo;
     });
